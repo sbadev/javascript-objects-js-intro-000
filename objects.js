79b45196aa8ad12f7e99597d38346bb0f118abbd
S A <sanden@sandenaverett.com>
2018-09-24 22:13:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ updatePlaylist(playlist, 'My Bloody Valentine', 'Sometimes');
 updatePlaylist(playlist, 'Phil Ochs', 'Here\'s to the State of Mississippi')
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
 
