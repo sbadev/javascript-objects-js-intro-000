93c6603e394e381462a3c81c40667f4f7cee9523
S A <sanden@sandenaverett.com>
2018-09-24 22:11:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,7 +11,6 @@ updatePlaylist(playlist, 'Phil Ochs', 'Here\'s to the State of Mississippi')
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
-  return playlist;
 }
 
 removeFromPlaylist(playlist, Slowdive);
