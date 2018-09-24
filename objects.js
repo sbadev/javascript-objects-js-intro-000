ad378da2fc8123ef47fcc0362ac3096b7aca3901
S A <sanden@sandenaverett.com>
2018-09-24 22:06:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { 'Imagine Dragons': 'Whatever It Takes' };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-    playlist[artistName]: songTitle
+    playlist[artistName] = songTitle
     return playlist;
 }
 
