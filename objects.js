4f2792ffeed7c8f38cbd62e767b4ad0e51ff855e
S A <sanden@sandenaverett.com>
2018-09-24 22:01:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,5 @@ var playlist = { 'Imagine Dragons': 'Whatever It Takes' };
 function updatePlaylist(playlist, artistName, songTitle) {
     return Object.assign({}, obj, { [artistName]: songTitle })
 }
+
+updatePlaylist(playlist, 'Slowdive', 'Alison');
